48aac5263f21669d087eae8808a1bf928dd085ee
pixinlei <2954716615@qq.com>
2020-12-07 21:58:22 +0800

    12-7

--- 1.js
+++ 1.js
@@ -1,1 +1,5 @@
-console.log('test')
+let arrayLike = { length: 2, 0: 'a', 1: 'b' };
+// 正确
+for (let x of Array.from(arrayLike)) {
+    console.log(x);
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# aiyong
+1.react的基本概念
+    REACT是Facebook开发出的js库，mvc无法满足需求
