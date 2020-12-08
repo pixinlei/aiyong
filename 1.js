ec224d7409e1d71ad2b2a073ad122d4c2b463d25
pixinlei <2954716615@qq.com>
2020-12-08 18:46:19 +0800

    第二次尝试ing

--- 1.js
+++
@@ -1,5 +0,0 @@
-let arrayLike = { length: 2, 0: 'a', 1: 'b' };
-// 正确
-for (let x of Array.from(arrayLike)) {
-    console.log(x);
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-1.react的基本概念
-    REACT是Facebook开发出的js库，mvc无法满足需求
